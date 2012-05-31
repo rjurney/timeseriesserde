d6f18eaf0f206f944bedd26702bad32ac6d53dbd
Russell Jurney <russell.jurney@gmail.com>
2012-05-30 20:38:17 -0700

    Time series serde

---
+++ src/TimeSeries.java
@@ -0,0 +1,21 @@
+package com.example.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public final class TimeSeries extends UDF {
+  public Text evaluate(final Integer i) {
+    if (i == null) { return null; }
+    String stars = new String();
+    if(i > 0) {
+      for(; i>0; i--) {
+        stars += "*";
+      }
+      return new Text(stars.toString());
+    }
+    else
+    {
+        return "";
+    }
+  }
+}
