cc77e8cb068cc9363ce72d80a1628a8fc6615a57
Russell Jurney <russell.jurney@gmail.com>
2012-06-01 14:23:09 -0700

    Hive serde works

--- src/TimeSeries.java
+++ src/TimeSeries.java
@@ -2,13 +2,14 @@ package com.example.hive.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
+import java.math.BigInteger;
 
 public final class TimeSeries extends UDF {
   public Text evaluate(final Integer i) {
     if (i == null) { return null; }
     String stars = new String();
     if(i > 0) {
-      for(; i>0; i--) {
+      for(Integer counter = 0; counter < i; counter++) {
         stars += "*";
       }
       return new Text(stars.toString());
