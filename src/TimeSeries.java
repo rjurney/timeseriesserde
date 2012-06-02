9dfad816d3de54f43cd8f99a0afa7ac93ec794a0
Russell Jurney <russell.jurney@gmail.com>
2012-06-01 18:07:15 -0700

    Fooberty

--- src/TimeSeries.java
+++ src/TimeSeries.java
@@ -3,14 +3,17 @@ package com.example.hive.udf;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 import java.math.BigInteger;
+import java.lang.Math;
 
 public final class TimeSeries extends UDF {
   public Text evaluate(final Integer i) {
     if (i == null) { return null; }
     String stars = new String();
     if(i > 0) {
-      for(Integer counter = 0; counter < i; counter++) {
-        stars += "*";
+      {
+        for(Integer counter = 0; counter < i; counter++) {
+          stars += "*";
+        }
       }
       return new Text(stars.toString());
     }
